6062a3541aef073c30f7e4b9ca1553573c3cbbc1
Shafali Gupta <43209217+shafali123@users.noreply.github.com>
2025-02-24 12:03:47 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SteganographyApp } from './steganography-app'
+
+function App() {
+  return (
+    <div className="bg-white min-h-screen flex items-center justify-center">
+      <SteganographyApp />
+    </div>
+  )
+}
+
+export default App
