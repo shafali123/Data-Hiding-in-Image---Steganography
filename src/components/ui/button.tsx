81106e9fb107e72d180280a768b947b180fa4ea5
Shafali Gupta <43209217+shafali123@users.noreply.github.com>
2025-02-24 12:07:07 +0530

    Create button.tsx

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Button = ({ children, className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
+  <button className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ${className}`} {...props}>
+    {children}
+  </button>
+)
